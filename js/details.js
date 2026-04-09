06669752ad9f471d16b2378c595d81e5a6b4ef2d
Sheika <sheika02x2@gmail.com>
2026-04-09 22:42:32 +0400

    Read course ID from URL using URLSearchParams

---
+++ js/details.js
@@ -0,0 +1,42 @@
+let courseId;
+let currentCourse;
+
+// قراءة id من الرابط
+const params = new URLSearchParams(window.location.search);
+courseId = parseInt(params.get("id"));
+
+// تحميل البيانات
+async function loadCourse() {
+  const res = await fetch("./data.json");
+  const data = await res.json();
+
+  // نجيب الكورس باستخدام for
+  for (let i = 0; i < data.courses.length; i++) {
+    if (data.courses[i].id === courseId) {
+      currentCourse = data.courses[i];
+      break;
+    }
+  }
+
+  renderCourse();
+  renderTopics();
+  renderInstructor();
+  renderEnroll();
+  renderQuiz();
+}
+
+loadCourse();
+
+// عرض بيانات الكورس
+function renderCourse() {
+  document.getElementById("courseHeader").innerHTML = `
+    <h2>${currentCourse.title}</h2>
+    <p>${currentCourse.instructor}</p>
+    <span class="badge bg-info">${currentCourse.category}</span>
+    <span class="badge bg-secondary">${currentCourse.level}</span>
+    <p>⭐ ${currentCourse.rating}</p>
+    <p>⏱ ${currentCourse.duration}</p>
+    <p>👨‍🎓 ${currentCourse.studentsCount}</p>
+  `;
+}
+
