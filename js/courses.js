a81f79452485487625cead50b5c6f0859b6812aa
Sheika <sheika02x2@gmail.com>
2026-04-09 11:46:13 +0400

    load cources

--- js/courses.js
+++ js/courses.js
@@ -2,3 +2,11 @@ let allCourses = [];
 let selectedCategory = "All";
 let selectedLevel = "All";
 let searchValue = "";
+
+async function loadCourses() {
+  const res = await fetch("./data.json");
+  const data = await res.json();
+
+  allCourses = data.courses;
+  renderCourses(allCourses);
+}
