7192e9c1cc73c6cc2d92f24413ddb19411e058c1
Sheika <sheika02x2@gmail.com>
2026-04-10 14:18:25 +0400

    save enrolled courses

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,3 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function getEnrolledCourses() {
   return JSON.parse(localStorage.getItem('enrolledCourses')) || [];
 }
+
+function saveEnrolledCourses(courses) {
+  localStorage.setItem('enrolledCourses', JSON.stringify(courses));
+}
